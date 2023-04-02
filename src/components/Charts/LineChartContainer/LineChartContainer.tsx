6f94266a93adbedb48c5c7f894cd8c4e2843e51d
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-04-02 17:54:01 +0200

    refactor: LineChartContainer

--- src/components/Charts/LineChartContainer/LineChartContainer.tsx
+++ src/components/Charts/LineChartContainer/LineChartContainer.tsx
@@ -1,30 +1,42 @@
 import { BaseCard } from "@/components/Cards";
 import { LineChart } from "../LineChart/LineChart";
-import { Component } from "solid-js";
+import {
+  Component,
+  createEffect,
+  createResource,
+  createSignal,
+} from "solid-js";
 import { AreaData, WhitespaceData } from "lightweight-charts";
+import { fetchCoinData } from "@/api/fetchCoinData";
+import { useCoinSwitch } from "@/context/coinSwitch";
+import { formatDataForLightchart } from "@/api/formatDataForLightchart";
 
-type LineChartContainerProps = {
-  data: (WhitespaceData | AreaData)[];
-  loading: boolean;
-  error: boolean;
-};
+export const LineChartContainer: Component = () => {
+  const [coin] = useCoinSwitch();
+  const [coinData] = createResource(() => coin.apiName, fetchCoinData);
+  const [chartData, setChartata] = createSignal<(WhitespaceData | AreaData)[]>(
+    []
+  );
 
-export const LineChartContainer: Component<LineChartContainerProps> = (
-  props
-) => {
+  createEffect(() => {
+    if (coinData()) {
+      setChartata(formatDataForLightchart(coinData()));
+    }
+  }, [coin]);
+
   return (
     <BaseCard bg="bg-dark-bg" p="">
-      {props.loading ? (
+      {coinData.loading ? (
         <div class="flex h-[20rem] w-full justify-center items-center text-white-font">
           Loading...
         </div>
       ) : null}
-      {props.error ? (
+      {coinData.error ? (
         <div class="flex h-[20rem] w-full justify-center items-center text-white-font">
           Chart API Error. Please refresh a page.
         </div>
       ) : null}
-      {!props.loading && !props.error && <LineChart data={props.data} />}
+      {!coinData.loading && !coinData.error && <LineChart data={chartData()} />}
     </BaseCard>
   );
 };
--- src/view/Withdraw/Withdraw.tsx
+++ src/view/Withdraw/Withdraw.tsx
@@ -7,32 +7,13 @@ import { AreaData, WhitespaceData } from "lightweight-charts";
 import { createEffect, createResource, createSignal } from "solid-js";
 
 export const Withdraw = () => {
-  const [coin, setCoin] = useCoinSwitch();
-  const [coinData, { refetch }] = createResource(
-    () => coin.apiName,
-    fetchCoinData
-  );
-  const [chartData, setChartata] = createSignal<(WhitespaceData | AreaData)[]>(
-    []
-  );
-
-  createEffect(() => {
-    if (coinData()) {
-      setChartata(formatDataForLightchart(coinData()));
-    }
-  }, [coin]);
-
   return (
     <div class="flex relative mt-20 mx-auto 2xl:w-[1280]">
       <div class="flex lg:w-1/4 md:w-1/2 w-full">
         <WithdrawForm />
       </div>
       <div class="w-3/4 ml-8">
-        <LineChartContainer
-          data={chartData()}
-          loading={coinData.loading}
-          error={coinData.error}
-        />
+        <LineChartContainer />
       </div>
     </div>
   );
