020dde13b5d5c19faaa434b72a0a2d713ace0a91
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2023-04-02 19:19:59 +0200

    refactor

--- src/view/Withdraw/Withdraw.tsx
+++ src/view/Withdraw/Withdraw.tsx
@@ -1,21 +1,13 @@
-import { fetchCoinData } from "@/api/fetchCoinData";
-import { formatDataForLightchart } from "@/api/formatDataForLightchart";
 import { LineChartContainer } from "@/components/Charts";
 import { WithdrawForm } from "@/components/Forms";
-import { useCoinSwitch } from "@/context/coinSwitch";
-import { AreaData, WhitespaceData } from "lightweight-charts";
-import { createEffect, createResource, createSignal } from "solid-js";
 
 export const Withdraw = () => {
   return (
-    // <div class="flex relative mt-20 mx-auto 2xl:w-[1280]">
     <div class="relative grid grid-cols-1 md:grid-cols-4  mt-20 mx-auto">
       <div class="flex md:col-span-2 xl:col-span-1">
-        {/* <div class="flex lg:w-1/4 md:w-1/2 w-full"> */}
         <WithdrawForm />
       </div>
       <div class="grid-span-1 md:col-span-2 xl:col-span-3 md:ml-8 sm:mt-0 mt-4">
-        {/* <div class="lg:w-3/4 ml-8 "> */}
         <LineChartContainer />
       </div>
     </div>
